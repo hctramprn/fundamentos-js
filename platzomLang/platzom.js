1940ae8c9f0434db96492a738616114121d2f1f7
Hector Amparan <hctramprn@users.noreply.github.com>
2017-05-23 23:56:52 -0500

    Regla del palíndromo funcional

---
+++ platzomLang/platzom.js
@@ -0,0 +1,35 @@
+function traductor() {
+  //Se importan los elementos desde el DOM
+  const s = document.getElementById("palabra")
+  const textoTraducido = document.getElementById("textoTraducido")
+  const str = s.value
+
+  //Si la palabra es un palíndromo, no aplica alternan entre mayúsculas y minúsculas, y no aplica ninguna otra regla
+  const reversa = (str) => str.toLowerCase().split('').reverse().join('')
+
+  function minMay(str) {
+    let mayuscula = true
+    const longitud = str.length
+    let texto = ''
+
+    for (let i = 0; i < longitud; i++) {
+      const caracter = str.charAt(i)
+      texto += mayuscula ? caracter.toUpperCase() : caracter.toLowerCase()
+      mayuscula = !mayuscula
+    }
+    return texto
+  }
+
+  if (str.toLowerCase() == reversa(str)) {
+    return minMay(str)
+  }
+
+  //Si la palabra termina en 'ar', se le quitan esos dos caracteres
+
+
+  //Si la palabra inicia con Z, se le añade 'pe' al final
+
+
+  //Si la palabra traducida tiene más de 10 caracteres, se parte a la mitad y se une con un guión en medio
+
+}
