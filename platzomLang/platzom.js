ad61d7416bda38caa5f6ba3c9ea7ff036d345b97
Hector Amparan <hctramprn@users.noreply.github.com>
2017-05-24 23:51:48 -0500

    Ahora se puede traducir todo un texto

--- platzomLang/platzom.js
+++ platzomLang/platzom.js
@@ -1,46 +1,52 @@
 function traductor() {
   //Se importan los elementos desde el DOM
-  const s = document.getElementById("palabra")
+  const s = document.getElementById("palabras")
   const textoTraducido = document.getElementById("textoTraducido")
-  const str = (s.value).toLowerCase()
-  let texto = str
+  const palabras = s.value.split(' ')
 
-  //Si la palabra es un palíndromo, no aplica alternan entre mayúsculas y minúsculas, y no aplica ninguna otra regla
-  const reversa = (str) => str.toLowerCase().split('').reverse().join('')
 
-  function minMay(str) {
-    let mayuscula = true
-    const longitud = str.length
-    let texto = ''
+  textoTraducido.innerHTML = `<strong>El texto traducido es el siguiente: </strong><br><br>`
+  for (str of palabras) {
 
-    for (let i = 0; i < longitud; i++) {
-      const caracter = str.charAt(i)
-      texto += mayuscula ? caracter.toUpperCase() : caracter.toLowerCase()
-      mayuscula = !mayuscula
+    let texto = str
+    //Si la palabra es un palíndromo, no aplica alternan entre mayúsculas y minúsculas, y no aplica ninguna otra regla
+    let reversa = (str) => str.toLowerCase().split('').reverse().join('')
+
+    function minMay(str) {
+      let mayuscula = true
+      let longitud = str.length
+      let texto = ''
+
+      for (let i = 0; i < longitud; i++) {
+        let caracter = str.charAt(i)
+        texto += mayuscula ? caracter.toUpperCase() : caracter.toLowerCase()
+        mayuscula = !mayuscula
+      }
+      textoTraducido.innerHTML += `${texto} `
     }
-    return textoTraducido.innerHTML = `La palabra traducida es: <strong>${texto}</strong>`
-  }
 
-  if (str.toLowerCase() == reversa(str)) {
-    return minMay(str)
-  }
+    if (str.toLowerCase() == reversa(str)) {
+      minMay(str)
+    }
+    else {
+      //Si la palabra termina en 'ar', se le quitan esos dos caracteres
+      if (str.endsWith('ar')) {
+        texto = str.slice(0, -2)
+      }
 
-  //Si la palabra termina en 'ar', se le quitan esos dos caracteres
-  if (str.endsWith('ar')) {
-    texto = str.slice(0, -2)
-  }
+      //Si la palabra inicia con Z, se le añade 'pe' al final
+      if (str.startsWith('z')) {
+        texto += 'pe'
+      }
 
-  //Si la palabra inicia con Z, se le añade 'pe' al final
-  if (str.startsWith('z')) {
-    texto += 'pe'
+      //Si la palabra traducida tiene más de 10 caracteres, se parte a la mitad y se une con un guión en medio
+      let long = texto.length
+      if (long >= 10) {
+        let primeraMitad = texto.slice(0, Math.round(long / 2))
+        let segundaMitad = texto.slice(Math.round(long / 2))
+        texto = `${primeraMitad}-${segundaMitad}`
+      }
+      textoTraducido.innerHTML += `${texto} `
+    }
   }
-
-  //Si la palabra traducida tiene más de 10 caracteres, se parte a la mitad y se une con un guión en medio
-  const long = texto.length
-  if (long >= 10) {
-    const primeraMitad = texto.slice(0, Math.round(long / 2))
-    const segundaMitad = texto.slice(Math.round(long / 2))
-    texto = `${primeraMitad}-${segundaMitad}`
-  }
-  return textoTraducido.innerHTML = `La palabra traducida es: <strong>${texto}</strong>`
 }
